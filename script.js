f1e230fc6d069f4c2f77d5fbfa085e03f76e3847
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-11-11 22:48:13 +0530

    t

--- script.js
+++ script.js
@@ -2,24 +2,25 @@ const input = document.querySelector('.input');
 const btn = document.querySelector('.btn');
 const notesContainer = document.getElementById('notesContainer');
 
-let notes = JSON.parse(localStorage.getItem("notes")) || [];
-showNotes();
+
 
 btn.addEventListener("click", addNotes);
 
 function addNotes() {
     const text = input.value.trim();
-    if (text === "") return;
+    if(text === "") return;
+   
+    
 
     const note = document.createElement('div');
-    // note
     note.classList.add('note');
 
-    note.innerHTML = `<p>${text}</p>`;
+    note.innerHTML = `<p>${text}</p>`
 
-   
     notesContainer.prepend(note);
-    input.value = "";
-}
+   
 
+    input.value = "";
+
+}
 
