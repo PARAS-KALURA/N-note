d541edb12072389cd9513cedb0f5367a6a482fbf
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2025-11-13 15:41:15 +0530

    edit

--- script.js
+++ script.js
@@ -8,30 +8,47 @@ btn.addEventListener("click", addNotes);
 
 function addNotes() {
     const text = input.value.trim();
-    if(text === "") return;
-   
-    
+    if (text === "") return;
+
+
 
     const note = document.createElement('div');
     note.classList.add('note');
 
     note.innerHTML = `<p>${text}</p>`
 
-   
+
     const dltBtn = document.createElement('button');
     dltBtn.classList.add("dlt-btn");
 
     dltBtn.addEventListener("click", () => {
-      note.remove()
+        note.remove()
     })
 
-    dltBtn.innerHTML = `<span>Delete</span>`
+    dltBtn.textContent = 'Delete'
 
+    const editBtn = document.createElement('button');
+    editBtn.classList.add("edit-btn");
+    editBtn.textContent = 'Edit'
+
+   editBtn.addEventListener("click", () => {
+    const newText = prompt("Enter:", note.querySelector('p').innerText);
+
+    if(newText !== null && newText.trim() !== "") {
+       note.querySelector('p').innerText = newText.trim();
+    }
+
+   })
+
+
+
+
+    note.appendChild(editBtn);
     note.appendChild(dltBtn);
 
     notesContainer.prepend(note);
-    
-   
+
+
 
     input.value = "";
 
